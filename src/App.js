db8154b92048f7dfc6bdabaa2ba8cab98403f87b
zach_liu <zach_liu@gss.com.tw>
2022-02-13 21:48:37 +0800

    Finish WaWeEat.v2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename={ process.env.PUBLIC_URL }>
       <ScrollToTop>
         <Navbar />
         <Switch>
